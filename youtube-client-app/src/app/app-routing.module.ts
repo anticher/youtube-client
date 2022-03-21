9c3e8289ebcb9eac0dd4f816be1e55f629be6947
anticher <anticherq@gmail.com>
2022-03-21 13:28:41 +0300

    feat: add login-page

--- youtube-client-app/src/app/app-routing.module.ts
+++ youtube-client-app/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoginPageComponent } from './auth/components/login-page/login-page.component';
 import { NotFoundComponent } from './not-found/components/not-found.component';
 import { MainComponent } from './youtube/components/main/main.component';
 
 const routes = [
   { path: '', component: MainComponent },
   { path: 'test', component: NotFoundComponent },
+  { path: 'login', component: LoginPageComponent },
 ];
 
 @NgModule({
