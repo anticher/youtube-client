72c9983a98fcedcc3211feb8c1464e7ddb92b04f
anticher <anticherq@gmail.com>
2022-03-21 12:12:45 +0300

    feat: add routing

---
+++ youtube-client-app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/components/not-found.component';
+import { MainComponent } from './youtube/components/main/main.component';
+
+
+const routes = [
+  { path: '', component: MainComponent },
+  { path: 'test', component: NotFoundComponent},
+]
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { YoutubeModule } from './youtube/youtube.module';
+import { NotFoundModule } from './not-found/not-found.module';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { YoutubeModule } from './youtube/youtube.module';
     HttpClientModule,
     CoreModule,
     YoutubeModule,
+    NotFoundModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
