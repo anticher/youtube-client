db8cdc26b61d9667507aab2deb183583bf9ce693
anticher <anticherq@gmail.com>
2022-03-29 10:47:45 +0300

    fix: models structure and naming

--- youtube-client-app/src/app/models/search-item.model.ts
+++ youtube-client-app/src/app/models/search-item.model.ts
@@ -1,40 +1,46 @@
-interface ThumbnailsType {
-  url: string,
-  width: number,
-  height: number
+interface Thumbnail {
+    url: string,
+    width: number,
+    height: number
 }
 
-export interface SearchItem {
-  kind: string,
-  etag: string,
-  id: string,
-  snippet: {
+interface Thumbnails {
+    default: Thumbnail,
+    medium: Thumbnail
+    high: Thumbnail,
+    standard: Thumbnail,
+    maxres: Thumbnail
+}
+
+interface Snippet {
     publishedAt: string,
     channelId: string,
     title: string,
     description: string,
-    thumbnails: {
-      default: ThumbnailsType,
-      medium: ThumbnailsType
-      high: ThumbnailsType,
-      standard: ThumbnailsType,
-      maxres: ThumbnailsType
-    },
+    thumbnails: Thumbnails,
     channelTitle: string,
     tags: string[],
     categoryId: string,
     liveBroadcastContent: string,
     localized: {
-      title: string,
-      description: string
+        title: string,
+        description: string
     },
     defaultAudioLanguage: string,
-  },
-  statistics: {
+}
+
+interface Statistics {
     viewCount: string,
     likeCount: string,
     dislikeCount: string,
     favoriteCount: string,
     commentCount: string
-  }
+}
+
+export interface SearchItem {
+    kind: string,
+    etag: string,
+    id: string,
+    snippet: Snippet,
+    statistics: Statistics
 }
