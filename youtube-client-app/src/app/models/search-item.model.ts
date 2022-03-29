0b8afcb186fb286a8f4888f979e2247156504e37
anticher <anticherq@gmail.com>
2022-03-29 13:14:20 +0300

    feat: implement structure in accordance with BEM methodology

--- youtube-client-app/src/app/models/search-item.model.ts
+++ youtube-client-app/src/app/models/search-item.model.ts
@@ -1,46 +1,46 @@
 interface Thumbnail {
-    url: string,
-    width: number,
-    height: number
+  url: string,
+  width: number,
+  height: number
 }
 
 interface Thumbnails {
-    default: Thumbnail,
-    medium: Thumbnail
-    high: Thumbnail,
-    standard: Thumbnail,
-    maxres: Thumbnail
+  default: Thumbnail,
+  medium: Thumbnail
+  high: Thumbnail,
+  standard: Thumbnail,
+  maxres: Thumbnail
 }
 
 interface Snippet {
-    publishedAt: string,
-    channelId: string,
+  publishedAt: string,
+  channelId: string,
+  title: string,
+  description: string,
+  thumbnails: Thumbnails,
+  channelTitle: string,
+  tags: string[],
+  categoryId: string,
+  liveBroadcastContent: string,
+  localized: {
     title: string,
-    description: string,
-    thumbnails: Thumbnails,
-    channelTitle: string,
-    tags: string[],
-    categoryId: string,
-    liveBroadcastContent: string,
-    localized: {
-        title: string,
-        description: string
-    },
-    defaultAudioLanguage: string,
+    description: string
+  },
+  defaultAudioLanguage: string,
 }
 
 interface Statistics {
-    viewCount: string,
-    likeCount: string,
-    dislikeCount: string,
-    favoriteCount: string,
-    commentCount: string
+  viewCount: string,
+  likeCount: string,
+  dislikeCount: string,
+  favoriteCount: string,
+  commentCount: string
 }
 
 export interface SearchItem {
-    kind: string,
-    etag: string,
-    id: string,
-    snippet: Snippet,
-    statistics: Statistics
+  kind: string,
+  etag: string,
+  id: string,
+  snippet: Snippet,
+  statistics: Statistics
 }
