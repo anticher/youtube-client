31485a7e730e9050d45bdc76752e04705b373b73
anticher <anticherq@gmail.com>
2022-03-31 20:51:24 +0300

    refactor: move up search components

--- youtube-client-app/src/app/youtube/youtube.module.ts
+++ youtube-client-app/src/app/youtube/youtube.module.ts
@@ -8,8 +8,8 @@ import { ProfileSettingsComponent } from './components/profile-settings/profile-
 import { ResultItemComponent } from './components/result-item/result-item.component';
 import { ResultComponent } from './components/result/result.component';
 import { SearchButtonComponent } from './components/search-button/search-button.component';
-import { SearchItemComponent } from './components/search/search-item/search-item.component';
-import { SearchSortingComponent } from './components/search/search-sorting/search-sorting.component';
+import { SearchItemComponent } from './components/search-item/search-item.component';
+import { SearchSortingComponent } from './components/search-sorting/search-sorting.component';
 import { SettingsButtonComponent } from './components/settings-button/settings-button.component';
 import { StatisticsItemComponent } from './components/statistics-item/statistics-item.component';
 import { FilterSearchResultsPipe } from './pipes/filter-search-results.pipe';
