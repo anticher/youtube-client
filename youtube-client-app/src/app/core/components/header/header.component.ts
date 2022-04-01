ffe11fc32701fa5c281fa496219f9a28a13014e1
anticher <anticherq@gmail.com>
2022-04-01 16:48:36 +0300

    feat: add test login/logout buttons with actions

--- youtube-client-app/src/app/core/components/header/header.component.ts
+++ youtube-client-app/src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,17 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   public isSortingVisible: boolean = false;
 
+  constructor(private auth: AuthService) {}
+
   public toggleDisplay(): void {
     this.isSortingVisible = !this.isSortingVisible;
+  }
+
+  public login(): void {
+    this.auth.login();
+  }
+
+  public logout(): void {
+    this.auth.logout();
   }
 }
