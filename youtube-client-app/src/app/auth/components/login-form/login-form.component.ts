981d39b3641cd5776d0d3a0761de34e5c6f973f4
anticher <anticherq@gmail.com>
2022-04-01 17:53:03 +0300

    refactor: styles and delete square/rectangle button classes

--- youtube-client-app/src/app/auth/components/login-form/login-form.component.ts
+++ youtube-client-app/src/app/auth/components/login-form/login-form.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../../services/auth.service';
 export class LoginFormComponent {
   constructor(private auth: AuthService) {}
 
-  login() {
+  public login(): void {
     this.auth.login();
   }
 }
