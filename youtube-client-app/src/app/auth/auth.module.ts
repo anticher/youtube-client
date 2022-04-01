322838be14d9c4aa0f5624dd22471003b1d50f14
anticher <anticherq@gmail.com>
2022-04-01 16:29:14 +0300

    feat: add login template

--- youtube-client-app/src/app/auth/auth.module.ts
+++ youtube-client-app/src/app/auth/auth.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    LoginFormComponent
+  ],
   imports: [
     CommonModule,
   ],
---
+++ youtube-client-app/src/app/auth/components/login-form/login-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent {
+  // constructor(private auth: AuthService) {}
+
+  // login() {
+  //   this.auth.login();
+  // }
+}
