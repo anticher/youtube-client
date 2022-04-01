708211bb0972cc74c2668d0923d595372f3f738c
anticher <anticherq@gmail.com>
2022-04-01 18:20:13 +0300

    refactor: result-item class

--- youtube-client-app/src/app/youtube/components/result-item/result-item.component.ts
+++ youtube-client-app/src/app/youtube/components/result-item/result-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { SearchItem, Statistics } from 'src/app/youtube/models/search-item.model';
 
 @Component({
@@ -6,8 +6,12 @@ import { SearchItem, Statistics } from 'src/app/youtube/models/search-item.model
   templateUrl: './result-item.component.html',
   styleUrls: ['./result-item.component.scss'],
 })
-export class ResultItemComponent implements OnInit {
-  @Input() item!: SearchItem;
+export class ResultItemComponent implements OnChanges {
+  @Input() set item(item: SearchItem) {
+    this._item = item;
+  };
+
+  private _item!: SearchItem;
 
   public itemId: string = '';
 
@@ -27,14 +31,16 @@ export class ResultItemComponent implements OnInit {
 
   public publishedDaysAgo: string = '';
 
-  public ngOnInit(): void {
-    if (this.item) {
-      this.itemId = this.item.id;
-      this.statistics = this.item.statistics;
-      this.channelTitle = this.item.snippet.channelTitle;
-      this.categoryId = this.item.snippet.categoryId;
-      this.mediumImageUrl = this.item.snippet.thumbnails.medium.url;
-      this.publishedDaysAgo = this.item.snippet.publishedAt;
-    }
+  public ngOnChanges(): void {
+      this.setItemInfo();
+  }
+
+  private setItemInfo(): void {
+    this.itemId = this._item.id;
+    this.statistics = this._item.statistics;
+    this.channelTitle = this._item.snippet.channelTitle;
+    this.categoryId = this._item.snippet.categoryId;
+    this.mediumImageUrl = this._item.snippet.thumbnails.medium.url;
+    this.publishedDaysAgo = this._item.snippet.publishedAt;
   }
 }
