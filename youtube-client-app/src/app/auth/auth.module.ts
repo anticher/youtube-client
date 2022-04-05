f671e8b7272b90bfd95f261aa5e9d9c61e57ff62
anticher <anticherq@gmail.com>
2022-04-05 16:08:03 +0300

    feat: add login form basic validations

--- youtube-client-app/src/app/auth/auth.module.ts
+++ youtube-client-app/src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { LoginFormComponent } from './components/login-form/login-form.component
   imports: [
     CommonModule,
     SharedModule,
+    ReactiveFormsModule,
   ],
 })
 export class AuthModule { }
--- youtube-client-app/src/app/auth/components/login-form/login-form.component.ts
+++ youtube-client-app/src/app/auth/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -7,9 +8,21 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
+  public form = new FormGroup({
+    login: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+  })
+  
   constructor(private auth: AuthService) {}
 
-  login() {
+  submit() {
+    console.log('submit click')
+    console.log(this.form.value)
     this.auth.login();
   }
 }
