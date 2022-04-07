3d1359d7eb6ba7c83c55a52ef8176bdb6c32fcca
anticher <anticherq@gmail.com>
2022-04-07 18:20:18 +0300

    feat: login and logout buttons reflect current login state

--- youtube-client-app/src/app/core/components/header/header.component.ts
+++ youtube-client-app/src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
@@ -6,10 +6,16 @@ import { AuthService } from 'src/app/auth/services/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   public isSortingVisible: boolean = false;
 
+  public isLoginHidden: boolean = false;
+
   constructor(private auth: AuthService) {}
+
+  ngOnInit() {
+    this.auth.loginSubject.subscribe((value) => this.isLoginHidden = value)
+  }
 
   public toggleDisplay(): void {
     this.isSortingVisible = !this.isSortingVisible;
